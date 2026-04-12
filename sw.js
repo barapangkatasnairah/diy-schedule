6d20463fc24205df5e96c986582c69dcd340f019
barapangkatasnairah <106016124+barapangkatasnairah@users.noreply.github.com>
2026-04-12 23:26:37 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,57 @@
+const CACHE_NAME = "diy-schedule-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon.png"
+];
+
+// INSTALL
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log("Caching app shell");
+        return cache.addAll(urlsToCache);
+      })
+  );
+  self.skipWaiting();
+});
+
+// ACTIVATE
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log("Deleting old cache:", cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// FETCH
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request)
+          .then(fetchRes => {
+            return caches.open(CACHE_NAME).then(cache => {
+              cache.put(event.request, fetchRes.clone());
+              return fetchRes;
+            });
+          })
+          .catch(() => {
+            // fallback (optional)
+            return caches.match("./index.html");
+          });
+      })
+  );
+});
